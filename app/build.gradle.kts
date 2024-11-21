be03782e69dfe38a5d9c83b39f33b4aa65f24e93
dev-akashc <chandrawadeakash@gmail.com>
2024-11-21 16:58:14 +0000

    Forgot to update version numbers in gradle file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.grating.recolldroid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
